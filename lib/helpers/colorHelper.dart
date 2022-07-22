f1acc2629d54d20ad34ec94a7b7dda49cca7338c
Breno1112 <contato.brenofachini@hotmail.com>
2022-07-22 09:40:20 -0300

    adding skills

--- lib/helpers/colorHelper.dart
+++ lib/helpers/colorHelper.dart
@@ -2,4 +2,5 @@ import 'package:flutter/material.dart';
 
 class ColorHelper {
   static const blueResumeColor = Color(0xFF080C33);
+  static const skillProgressIndicatorBackgroundColor = Color(0xFF4C5056);
 }
--- lib/pages/resume.dart
+++ lib/pages/resume.dart
@@ -60,184 +60,7 @@ class ResumeState extends State<Resume> {
       color: ColorHelper.blueResumeColor,
       child: ListView(
         padding: const EdgeInsets.symmetric(horizontal: 20, vertical: 40),
-        children: [
-          SizedBox(
-            width: query.size.width * 0.4,
-            height: query.size.height * 0.4,
-            child: CircleAvatar(
-              radius: 1,
-              backgroundImage: AssetImage(data?["profile_image"]),
-            ),
-          ),
-          SizedBox(
-            height: query.size.width * 0.1,
-            width: query.size.width,
-          ),
-          FittedBox(
-            fit: BoxFit.cover,
-            child: Text(
-              data?["name"],
-              style: const TextStyle(
-                  color: Colors.white, fontWeight: FontWeight.bold),
-            ),
-          ),
-          SizedBox(
-            height: query.size.width * 0.1,
-            width: query.size.width,
-          ),
-          Padding(
-            padding: EdgeInsets.symmetric(horizontal: query.size.width * 0.1),
-            child: SizedBox(
-              height: query.size.height * 0.05,
-              width: query.size.width,
-              child: FittedBox(
-                fit: BoxFit.contain,
-                child: Text(
-                  data?["profession_shortname"],
-                  style: const TextStyle(color: Colors.white),
-                ),
-              ),
-            ),
-          ),
-          SizedBox(
-            height: query.size.width * 0.1,
-            width: query.size.width,
-          ),
-          RichText(
-            textAlign: TextAlign.center,
-            text: TextSpan(
-                text: data?["short_description"],
-                style: const TextStyle(color: Colors.white, fontSize: 20)),
-          ),
-          SizedBox(
-            height: query.size.width * 0.2,
-            width: query.size.width,
-          ),
-          Padding(
-            padding: EdgeInsets.symmetric(horizontal: query.size.width * 0.2),
-            child: const FittedBox(
-              fit: BoxFit.cover,
-              child: Text(
-                "Contact me",
-                style:
-                    TextStyle(color: Colors.white, fontWeight: FontWeight.bold),
-              ),
-            ),
-          ),
-          SizedBox(
-            height: query.size.width * 0.1,
-            width: query.size.width,
-          ),
-          GestureDetector(
-            onTap: () => sendMail(data?["email"]),
-            child: Row(
-              children: [
-                const Expanded(
-                  flex: 1,
-                  child: Icon(
-                    Icons.email_outlined,
-                    color: Colors.white,
-                  ),
-                ),
-                Expanded(
-                  flex: 7,
-                  child: FittedBox(
-                    fit: BoxFit.fitWidth,
-                    child: Text(
-                      data?["email"],
-                      style: const TextStyle(color: Colors.white),
-                      textAlign: TextAlign.start,
-                    ),
-                  ),
-                )
-              ],
-            ),
-          ),
-          SizedBox(
-            height: query.size.width * 0.1,
-            width: query.size.width,
-          ),
-          GestureDetector(
-            onTap: () => callMe(data?["phone"]),
-            child: Row(
-              children: [
-                const Expanded(
-                  flex: 1,
-                  child: Icon(
-                    Icons.phone_outlined,
-                    color: Colors.white,
-                  ),
-                ),
-                Expanded(
-                  flex: 7,
-                  child: Text(
-                    data?["phone"],
-                    style: const TextStyle(color: Colors.white, fontSize: 20),
-                    textAlign: TextAlign.start,
-                  ),
-                ),
-              ],
-            ),
-          ),
-          SizedBox(
-            height: query.size.width * 0.1,
-            width: query.size.width,
-          ),
-          GestureDetector(
-            onTap: () => navigateToMe(data?["maps_location"]),
-            child: Row(
-              children: [
-                const Expanded(
-                  flex: 1,
-                  child: Icon(
-                    Icons.location_on_outlined,
-                    color: Colors.white,
-                  ),
-                ),
-                Expanded(
-                  flex: 7,
-                  child: Text(
-                    data?["location"],
-                    style: const TextStyle(color: Colors.white, fontSize: 20),
-                    textAlign: TextAlign.start,
-                  ),
-                )
-              ],
-            ),
-          ),
-          SizedBox(
-            height: query.size.width * 0.1,
-            width: query.size.width,
-          ),
-          GestureDetector(
-            onDoubleTap: () => openWebsite(data?["website"]),
-            child: Row(
-              children: [
-                const Expanded(
-                  flex: 1,
-                  child: Icon(
-                    IconData(0xf68d,
-                        fontFamily: CupertinoIcons.iconFont,
-                        fontPackage:
-                            CupertinoIcons.iconFontPackage), //globe icon
-                    color: Colors.white,
-                  ),
-                ),
-                Expanded(
-                  flex: 7,
-                  child: FittedBox(
-                    fit: BoxFit.contain,
-                    child: Text(
-                      data?["website"],
-                      style: const TextStyle(color: Colors.white),
-                      textAlign: TextAlign.start,
-                    ),
-                  ),
-                )
-              ],
-            ),
-          )
-        ],
+        children: getMobileWidgetList(query, data),
       ),
     );
   }
@@ -303,11 +126,236 @@ class ResumeState extends State<Resume> {
     await launchUrl(Uri.parse("tel:$phone"));
   }
 
-  navigateToMe(String mapsLocation) async {
-    await launchUrl(Uri.parse(mapsLocation));
-  }
-
   openWebsite(String website) async {
     await launchUrl(Uri.parse(website));
+  }
+
+  List<Widget> buildWidgetSkills(MediaQueryData query, dynamic e) {
+    List<dynamic> l = List<dynamic>.from(e);
+    List<Map<String, dynamic>> skills = [];
+    for (var element in l) {
+      Map<String, dynamic> m = Map<String, dynamic>.from(element);
+      skills.add(m);
+    }
+    // e.forEach((element) {
+    //   Map<String, dynamic> m = element as Map<String, dynamic>;
+    //   skills.add(m);
+    // });
+    List<Widget> result = [];
+    for (var element in skills) {
+      result.add(Row(
+        children: [
+          Expanded(
+            child: Padding(
+              padding: const EdgeInsets.symmetric(horizontal: 10),
+              child: FittedBox(
+                alignment: Alignment.centerLeft,
+                fit: BoxFit.scaleDown,
+                child: Text(
+                  element["name"],
+                  textAlign: TextAlign.start,
+                  style: const TextStyle(color: Colors.white, fontSize: 20),
+                ),
+              ),
+            ),
+          ),
+          Expanded(
+            child: ClipRRect(
+              borderRadius: const BorderRadius.horizontal(
+                  left: Radius.circular(20), right: Radius.circular(20)),
+              child: LinearProgressIndicator(
+                value: element["value"],
+                color: Colors.white,
+                backgroundColor:
+                    ColorHelper.skillProgressIndicatorBackgroundColor,
+                minHeight: 10,
+              ),
+            ),
+          )
+        ],
+      ));
+      result.add(SizedBox(
+        height: query.size.width * 0.1,
+        width: query.size.width,
+      ));
+    }
+    return result;
+  }
+
+  List<Widget> getMobileWidgetList(
+      MediaQueryData query, Map<String, dynamic>? data) {
+    List<Widget> result = [
+      SizedBox(
+        width: query.size.width * 0.4,
+        height: query.size.height * 0.4,
+        child: CircleAvatar(
+          radius: 1,
+          backgroundImage: AssetImage(data?["profile_image"]),
+        ),
+      ),
+      SizedBox(
+        height: query.size.width * 0.1,
+        width: query.size.width,
+      ),
+      FittedBox(
+        fit: BoxFit.cover,
+        child: Text(
+          data?["name"],
+          style:
+              const TextStyle(color: Colors.white, fontWeight: FontWeight.bold),
+        ),
+      ),
+      SizedBox(
+        height: query.size.width * 0.1,
+        width: query.size.width,
+      ),
+      Padding(
+        padding: EdgeInsets.symmetric(horizontal: query.size.width * 0.1),
+        child: SizedBox(
+          height: query.size.height * 0.05,
+          width: query.size.width,
+          child: FittedBox(
+            fit: BoxFit.contain,
+            child: Text(
+              data?["profession_shortname"],
+              style: const TextStyle(color: Colors.white),
+            ),
+          ),
+        ),
+      ),
+      SizedBox(
+        height: query.size.width * 0.1,
+        width: query.size.width,
+      ),
+      RichText(
+        textAlign: TextAlign.center,
+        text: TextSpan(
+            text: data?["short_description"],
+            style: const TextStyle(color: Colors.white, fontSize: 20)),
+      ),
+      SizedBox(
+        height: query.size.width * 0.2,
+        width: query.size.width,
+      ),
+      Padding(
+        padding: EdgeInsets.symmetric(horizontal: query.size.width * 0.2),
+        child: const FittedBox(
+          fit: BoxFit.cover,
+          child: Text(
+            "Contact me",
+            style: TextStyle(color: Colors.white, fontWeight: FontWeight.bold),
+          ),
+        ),
+      ),
+      SizedBox(
+        height: query.size.width * 0.1,
+        width: query.size.width,
+      ),
+      GestureDetector(
+        onTap: () => sendMail(data?["email"]),
+        child: Row(
+          children: [
+            const Expanded(
+              flex: 1,
+              child: Icon(
+                Icons.email_outlined,
+                color: Colors.white,
+              ),
+            ),
+            Expanded(
+              flex: 7,
+              child: FittedBox(
+                fit: BoxFit.fitWidth,
+                child: Text(
+                  data?["email"],
+                  style: const TextStyle(color: Colors.white),
+                  textAlign: TextAlign.start,
+                ),
+              ),
+            )
+          ],
+        ),
+      ),
+      SizedBox(
+        height: query.size.width * 0.1,
+        width: query.size.width,
+      ),
+      GestureDetector(
+        onTap: () => callMe(data?["phone"]),
+        child: Row(
+          children: [
+            const Expanded(
+              flex: 1,
+              child: Icon(
+                Icons.phone_outlined,
+                color: Colors.white,
+              ),
+            ),
+            Expanded(
+              flex: 7,
+              child: Text(
+                data?["phone"],
+                style: const TextStyle(color: Colors.white, fontSize: 20),
+                textAlign: TextAlign.start,
+              ),
+            ),
+          ],
+        ),
+      ),
+      SizedBox(
+        height: query.size.width * 0.1,
+        width: query.size.width,
+      ),
+      GestureDetector(
+        onDoubleTap: () => openWebsite(data?["website"]),
+        child: Row(
+          children: [
+            const Expanded(
+              flex: 1,
+              child: Icon(
+                IconData(0xf68d,
+                    fontFamily: CupertinoIcons.iconFont,
+                    fontPackage: CupertinoIcons.iconFontPackage), //globe icon
+                color: Colors.white,
+              ),
+            ),
+            Expanded(
+              flex: 7,
+              child: FittedBox(
+                fit: BoxFit.contain,
+                child: Text(
+                  data?["website"],
+                  style: const TextStyle(color: Colors.white),
+                  textAlign: TextAlign.start,
+                ),
+              ),
+            )
+          ],
+        ),
+      ),
+      SizedBox(
+        height: query.size.width * 0.2,
+        width: query.size.width,
+      ),
+      Padding(
+        padding: EdgeInsets.symmetric(horizontal: query.size.width * 0.32),
+        child: const FittedBox(
+          fit: BoxFit.cover,
+          child: Text(
+            "Skills",
+            style: TextStyle(color: Colors.white, fontWeight: FontWeight.bold),
+          ),
+        ),
+      ),
+      SizedBox(
+        height: query.size.width * 0.1,
+        width: query.size.width,
+      ),
+    ];
+    var skilWidgetList = buildWidgetSkills(query, data?["skills"]);
+    for (var element in skilWidgetList) {
+      result.add(element);
+    }
+    return result;
   }
 }
