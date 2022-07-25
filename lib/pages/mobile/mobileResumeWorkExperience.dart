b78f5cb743d4976ec5ca77acc48eb0e6da7a9067
Breno1112 <contato.brenofachini@hotmail.com>
2022-07-24 22:46:25 -0300

    adding bulleted list item

--- lib/pages/mobile/mobileResumeWorkExperience.dart
+++ lib/pages/mobile/mobileResumeWorkExperience.dart
@@ -5,6 +5,16 @@ import '../../helpers/colorHelper.dart';
 class MobileResumeWorkExperience extends StatelessWidget {
   const MobileResumeWorkExperience({Key? key}) : super(key: key);
 
+  Widget bullet() {
+    return Container(
+        height: 10.0,
+        width: 10.0,
+        decoration: const BoxDecoration(
+          color: ColorHelper.blueResumeColor,
+          shape: BoxShape.circle,
+        ));
+  }
+
   @override
   Widget build(BuildContext context) {
     var query = MediaQuery.of(context);
@@ -16,58 +26,66 @@ class MobileResumeWorkExperience extends StatelessWidget {
       ),
       body: Container(
         color: Colors.white,
-        child: Padding(
+        child: ListView(
           padding: const EdgeInsets.symmetric(horizontal: 20),
-          child: ListView(
-            padding: const EdgeInsets.symmetric(horizontal: 20, vertical: 20),
-            children: [
-              Hero(
-                  tag: "mobile-resume-contents",
-                  child: Padding(
-                    padding: const EdgeInsets.fromLTRB(10, 0, 0, 0),
-                    child: Row(
-                      children: const [
-                        Expanded(
-                          flex: 1,
-                          child: FittedBox(
-                            fit: BoxFit.cover,
-                            child: Text(
-                              "Education",
-                              style: TextStyle(
-                                  color: ColorHelper.blueResumeColor,
-                                  fontWeight: FontWeight.bold),
-                            ),
+          children: [
+            Hero(
+                tag: "mobile-resume-contents",
+                child: Padding(
+                  padding: const EdgeInsets.fromLTRB(20, 0, 0, 0),
+                  child: Row(
+                    children: const [
+                      Expanded(
+                        flex: 1,
+                        child: FittedBox(
+                          fit: BoxFit.cover,
+                          child: Text(
+                            "Education",
+                            style: TextStyle(
+                                color: ColorHelper.blueResumeColor,
+                                fontWeight: FontWeight.bold),
                           ),
                         ),
-                        Expanded(
-                          flex: 1,
-                          child: SizedBox(),
-                        )
-                      ],
-                    ),
-                  )),
-              SizedBox(
-                width: query.size.width,
-                height: query.size.height * 0.1,
-              ),
-              Row(
-                children: [
-                  Text(
-                    ".",
-                    style: TextStyle(fontSize: 80),
-                  )
-                ],
-              ),
-              Row(
-                children: [
-                  VerticalDivider(
+                      ),
+                      Expanded(
+                        flex: 1,
+                        child: SizedBox(),
+                      )
+                    ],
+                  ),
+                )),
+            SizedBox(
+              width: query.size.width,
+              height: query.size.height * 0.1,
+            ),
+            Row(
+              children: [
+                Padding(
+                  padding: EdgeInsets.fromLTRB(2.5, 0, 0, 0),
+                  child: bullet(),
+                )
+              ],
+            ),
+            Row(
+              children: [
+                SizedBox(
+                  height: query.size.height * 0.1,
+                  child: VerticalDivider(
                     thickness: 2,
                     color: ColorHelper.blueResumeColor,
-                  )
-                ],
-              )
-            ],
-          ),
+                  ),
+                )
+              ],
+            ),
+            Row(
+              children: [
+                Padding(
+                  padding: EdgeInsets.fromLTRB(2.5, 0, 0, 0),
+                  child: bullet(),
+                )
+              ],
+            ),
+          ],
         ),
       ),
     );
