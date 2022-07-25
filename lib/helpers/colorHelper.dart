823726bacc1db3fdd15f46a1b83f3a9d4815351f
Breno1112 <contato.brenofachini@hotmail.com>
2022-07-25 09:31:21 -0300

    ready to publish

--- lib/helpers/colorHelper.dart
+++
@@ -1,6 +0,0 @@
-import 'package:flutter/material.dart';
-
-class ColorHelper {
-  static const blueResumeColor = Color(0xFF080C33);
-  static const skillProgressIndicatorBackgroundColor = Color(0xFF4C5056);
-}
