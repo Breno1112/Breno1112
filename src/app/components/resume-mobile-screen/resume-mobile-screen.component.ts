93dde9cf277f68b921708ccbd7645a5ded844786
Breno1112 <contato.brenofachini@hotmail.com>
2022-08-17 15:06:24 -0300

    finishing up two sections

--- src/app/components/resume-mobile-screen/resume-mobile-screen.component.ts
+++ src/app/components/resume-mobile-screen/resume-mobile-screen.component.ts
@@ -1,4 +1,6 @@
 import { Component, Input, OnInit } from '@angular/core';
+import { MatIconRegistry } from '@angular/material/icon';
+import { DomSanitizer } from '@angular/platform-browser';
 
 @Component({
   selector: 'app-resume-mobile-screen',
@@ -8,11 +10,25 @@ import { Component, Input, OnInit } from '@angular/core';
 export class ResumeMobileScreenComponent implements OnInit {
   @Input() data: any;
   public profile_image: string = "";
-  constructor() { }
+  constructor(
+    private matIconRegistry: MatIconRegistry,
+    private domSanitizer: DomSanitizer
+  ) { 
+    this.matIconRegistry.addSvgIcon(
+      "trophy",
+      this.domSanitizer.bypassSecurityTrustResourceUrl("assets/icons/trophy.svg")
+    );
+  }
 
   ngOnInit(): void {
     this.profile_image = this.data["profile_image"];
     console.log(this.data);
+  }
+
+  parse_date(str: string) {
+    const result = new Date(str);
+    result.setDate(result.getDate() + 1);
+    return result;
   }
 
 }
