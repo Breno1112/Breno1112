d1c663868dfc9ef6bfabb48a8dac81042407e2a8
Breno1112 <contato.brenofachini@hotmail.com>
2022-08-20 01:35:37 -0300

    adding work details. Need to fill in data afterwards

--- src/app/components/profissional-info/profissional-info.component.ts
+++ src/app/components/profissional-info/profissional-info.component.ts
@@ -1,6 +1,9 @@
+import { NestedTreeControl } from '@angular/cdk/tree';
 import { Component, Input, OnInit } from '@angular/core';
 import { MatIconRegistry } from '@angular/material/icon';
+import { MatTreeNestedDataSource } from '@angular/material/tree';
 import { DomSanitizer } from '@angular/platform-browser';
+import { WorkExperience } from 'src/app/types/work.experience.type';
 
 @Component({
   selector: 'app-profissional-info',
@@ -9,6 +12,9 @@ import { DomSanitizer } from '@angular/platform-browser';
 })
 export class ProfissionalInfoComponent implements OnInit {
   @Input() data: any;
+
+  public treeControl = new NestedTreeControl<WorkExperience>(node => node.tasks);
+  public dataSource = new MatTreeNestedDataSource<WorkExperience>();
 
   constructor(
     private matIconRegistry: MatIconRegistry,
@@ -25,6 +31,7 @@ export class ProfissionalInfoComponent implements OnInit {
   }
 
   ngOnInit(): void {
+    this.dataSource = this.data["work_experience"];
   }
 
   parse_date(str: string) {
@@ -32,5 +39,7 @@ export class ProfissionalInfoComponent implements OnInit {
     result.setDate(result.getDate() + 1);
     return result;
   }
+
+  hasChild = (_: number, node: WorkExperience) => !!node.tasks && node.tasks.length > 0;
 
 }
---
+++ src/app/types/work.experience.type.ts
@@ -0,0 +1,8 @@
+export type WorkExperience = {
+    job_title: string,
+    company_name: string,
+    start_date: string,
+    end_date: string,
+    description: string,
+    tasks: WorkExperience[]
+};
