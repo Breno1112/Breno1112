c7c32aaa63d035cf84b1fc596677f3526b258376
Breno1112 <contato.brenofachini@hotmail.com>
2022-08-22 17:03:28 -0300

    starting to build wide desktop layout

--- src/app/components/resume-wide-screen/resume-wide-screen.component.ts
+++ src/app/components/resume-wide-screen/resume-wide-screen.component.ts
@@ -6,12 +6,21 @@ import { Component, Input, OnInit } from '@angular/core';
   styleUrls: ['./resume-wide-screen.component.css']
 })
 export class ResumeWideScreenComponent implements OnInit {
+  private lastCalculatedLeastHeight: number | undefined = undefined;
 
   constructor() { }
 
   @Input("data") public data: any;
 
   ngOnInit(): void {
+  }
+
+  calculateHeight() {
+    const got = window.innerHeight;
+    if(!this.lastCalculatedLeastHeight || got < this.lastCalculatedLeastHeight) {
+      this.lastCalculatedLeastHeight = got;
+    }
+    return `${this.lastCalculatedLeastHeight}px`;
   }
 
 }
